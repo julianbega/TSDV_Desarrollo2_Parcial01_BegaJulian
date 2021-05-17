11d58fbb77b5032216d67c1a00a3351da8c4fa54
Julian Bega <julian.begamilrad@gmail.com>
2021-05-17 12:48:21 -0300

    PlayerCollideWellWithMap

--- BombermanBegaJulian/Assets/Scripts/PlayerManager.cs
+++ BombermanBegaJulian/Assets/Scripts/PlayerManager.cs
@@ -7,9 +7,10 @@ public class PlayerManager : MonoBehaviour
 {
     public int playerLives = 3;
     public float speed = 1;
+    private GameManager gameManager;
     void Start()
     {
-        
+        gameManager = FindObjectOfType<GameManager>();
     }
 
     // Update is called once per frame
@@ -21,22 +22,22 @@ public class PlayerManager : MonoBehaviour
 
     void PlayerMovment()
     {
-        if (Input.GetKey(KeyCode.LeftArrow) && ((float)Math.Round(transform.position.x, 0) % 2 != 0))
+        if (Input.GetKey(KeyCode.LeftArrow) && ((float)Math.Round(transform.position.x, 0) % 2 != 0) && transform.position.z <= gameManager.mapColumn-2)
         {
             transform.position += Vector3.forward * speed * Time.deltaTime;
             transform.position = new Vector3((float)Math.Round(transform.position.x, 0), transform.position.y, transform.position.z);
         }
-        if (Input.GetKey(KeyCode.RightArrow) && ((float)Math.Round(transform.position.x, 0) % 2 != 0))
+        if (Input.GetKey(KeyCode.RightArrow) && ((float)Math.Round(transform.position.x, 0) % 2 != 0) && transform.position.z >=1)
         {
             transform.position += Vector3.back * speed * Time.deltaTime;
             transform.position = new Vector3((float)Math.Round(transform.position.x, 0), transform.position.y, transform.position.z);
         }
-        if (Input.GetKey(KeyCode.UpArrow) && ((float)Math.Round(transform.position.z, 0) % 2 != 0))
+        if (Input.GetKey(KeyCode.UpArrow) && ((float)Math.Round(transform.position.z, 0) % 2 != 0) && transform.position.x <= gameManager.mapRows-2)
         {
             transform.position += Vector3.right * speed * Time.deltaTime;
             transform.position = new Vector3(transform.position.x, transform.position.y, (float)Math.Round(transform.position.z, 0));
         }
-        if (Input.GetKey(KeyCode.DownArrow) && ((float)Math.Round(transform.position.z, 0) % 2 != 0))
+        if (Input.GetKey(KeyCode.DownArrow) && ((float)Math.Round(transform.position.z, 0) % 2 != 0) && transform.position.x >= 1)
         {
             transform.position += Vector3.left * speed * Time.deltaTime;
             transform.position = new Vector3(transform.position.x, transform.position.y, (float)Math.Round(transform.position.z, 0));
