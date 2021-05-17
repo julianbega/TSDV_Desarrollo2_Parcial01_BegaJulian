e099e71ffbd73aad5f272e4eb5c120bbcbcc9e85
Julian Bega <julian.begamilrad@gmail.com>
2021-05-17 20:58:44 -0300

    CameraController

--- BombermanBegaJulian/Assets/Scripts/CameraController.cs
+++ BombermanBegaJulian/Assets/Scripts/CameraController.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] public PlayerManager lookAtThat;
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] [Range(5, 15)] public float verticalDistance;
+    [SerializeField] [Range(3, 10)] public float horizontalDistanceX;
+    [SerializeField] [Range(-3, 3)] public float horizontalDistanceZ;
+    public float smothSpeed;
+    private Vector3 zoom;
+    bool start = false;
+
+    private Vector3 posToMoveTowards;
+
+    private void Start()
     {
-        
+        smothSpeed = 5;
+    }
+    void LateUpdate()
+    {
+        if (start == false)
+        {
+            start = true;
+            lookAtThat = FindObjectOfType<PlayerManager>();
+        }
+        FocusToTargetAndMove();
+    }
+    public void FocusToTargetAndMove()
+    {
+        Vector3 myPos = transform.position;
+
+        zoom = new Vector3(-horizontalDistanceX, verticalDistance, horizontalDistanceZ);
+
+        posToMoveTowards = lookAtThat.transform.position + zoom;
+
+       transform.LookAt(lookAtThat.transform);
+
+        transform.position = Vector3.Lerp(myPos, posToMoveTowards, Vector3.Distance(myPos, posToMoveTowards) * Time.deltaTime * smothSpeed);
     }
 }
