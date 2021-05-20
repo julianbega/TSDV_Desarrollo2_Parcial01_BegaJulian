f604a95ea69bfb0038049453e5352efd08de498c
Julian Bega <julian.begamilrad@gmail.com>
2021-05-20 19:34:32 -0300

    DoorPrefab

---
+++ BombermanBegaJulian/Assets/Scripts/Door.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public bool isDoorOpen;
+    public bool endGame;
+    void Start()
+    {
+        isDoorOpen = false;
+        GameManager.checkOpenDoor += OpenDoor;
+    }
+
+    private void OnDisable()
+    {
+        GameManager.checkOpenDoor -= OpenDoor;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && isDoorOpen == true)
+        {
+            endGame = true;
+        }
+    }
+
+    private void OpenDoor()
+    {
+        isDoorOpen = true;
+    }
+}
